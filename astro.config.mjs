8ed697caf337f53d71a64ceea684e647ca6e0128
luojinan <luojinan@zhuanzhuan.com>
2026-03-25 19:15:50 +0800

    feat: 支持 vitepress 语法 :::detail:::

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkFlexibleContainers from 'remark-flexible-containers';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  markdown: {
+    remarkPlugins: [remarkFlexibleContainers],
+  },
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
